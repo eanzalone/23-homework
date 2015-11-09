3b6e4d79c0879d227f398948ae65ca968b7d7b45
eanzalone <emily.anzalone@gmail.com>
2015-11-09 14:38:36 -0800

    Fixed sub-repo error

---
+++ eanzalone/week01/tic-tac-toe/ticTacToe.js
@@ -0,0 +1,11 @@
+// wait for the DOM to finish loading
+$(document).ready(function() {
+  console.log("JS is working.");
+
+  //who's turn
+  //what happens when a box is clicked
+  	//don't let x/o class be removed without reset button -- no toggle
+  //switch turn
+
+
+});
