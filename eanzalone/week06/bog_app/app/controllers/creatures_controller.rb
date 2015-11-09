a9f60ef36898d9548755efb81c12a973bae757ff
eanzalone <emily.anzalone@gmail.com>
2015-11-09 12:50:31 -0800

    Bog App

---
+++ eanzalone/week06/bog_app/app/controllers/creatures_controller.rb
@@ -0,0 +1,36 @@
+class CreaturesController < ApplicationController
+	def index
+	  # get all creatures from db and save to instance variable
+	  @creatures = Creature.all  
+	  # render index view file (it will have access to instance variables)
+	  render :index
+	end
+
+	def new
+	  @creature = Creature.new
+	  render :new
+	end
+
+	def create
+	  # validate params and save them as a variable
+		creature_params = params.require(:creature).permit(:name, :description)
+		# create a new creature with those params
+		creature = Creature.new(creature_params)
+		# check that it saved
+		if creature.save
+			# if saved, redirect to route that shows just this creature
+			redirect_to creature
+			# ^ same as redirect_to creature_path(creature)
+			# ^ same as redirect_to "/creatures/#{creature.id}"
+		end
+	end
+
+	def show
+	  # get the id parameter from the url
+	  id = params[:id]
+	  # find the creature with that id and save to an instance variable
+	  @creature = Creature.find(id)
+	  # render the show page -- it will have access to the @creature variable
+	  render :show
+	end
+end
---
+++ eanzalone/week06/bog_app/config/routes.rb
@@ -0,0 +1,66 @@
+Rails.application.routes.draw do
+  root to: 'creatures#index'
+  # We'll also use the resources method to have Rails 
+  # make an index route for our creatures resource.
+  resources :creatures, only: [:index, :new, :create, :show]
+  # resources :creatures with :index is equivalent to adding:
+  # get "/creatures", to: "creatures#index"
+
+
+
+### CAME WITH RAILS TEXT ###
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
+
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+  # Example resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Example resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Example resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Example resource route with more complex sub-resources:
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', on: :collection
+  #     end
+  #   end
+
+  # Example resource route with concerns:
+  #   concern :toggleable do
+  #     post 'toggle'
+  #   end
+  #   resources :posts, concerns: :toggleable
+  #   resources :photos, concerns: :toggleable
+
+  # Example resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+end
